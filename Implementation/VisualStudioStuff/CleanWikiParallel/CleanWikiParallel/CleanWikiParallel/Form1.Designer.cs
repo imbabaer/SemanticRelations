47407830fc5008306d5aa67fb00d048a9f8dab34
Ruben <rubenmueller90@gmail.com>
2015-06-27 17:07:26 +0200

    visualstudio zeug hinzugefügt
    schaubild cbowvsskipgram hinzugefügt
    thesis weiter geschrieben

---
+++ Implementation/VisualStudioStuff/CleanWikiParallel/CleanWikiParallel/CleanWikiParallel/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CleanWikiParallel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.textBoxStart = new System.Windows.Forms.TextBox();
+            this.textBoxEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelFiles = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelProcessed = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxSearchReturn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(75, 10);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxStart
+            // 
+            this.textBoxStart.Location = new System.Drawing.Point(75, 36);
+            this.textBoxStart.Name = "textBoxStart";
+            this.textBoxStart.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStart.TabIndex = 2;
+            this.textBoxStart.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBoxEnd
+            // 
+            this.textBoxEnd.Location = new System.Drawing.Point(75, 62);
+            this.textBoxEnd.Name = "textBoxEnd";
+            this.textBoxEnd.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEnd.TabIndex = 3;
+            this.textBoxEnd.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Start";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "End";
+            // 
+            // labelFiles
+            // 
+            this.labelFiles.AutoSize = true;
+            this.labelFiles.Location = new System.Drawing.Point(72, 85);
+            this.labelFiles.Name = "labelFiles";
+            this.labelFiles.Size = new System.Drawing.Size(36, 13);
+            this.labelFiles.TabIndex = 7;
+            this.labelFiles.Text = "Emptz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Files";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Processed";
+            // 
+            // labelProcessed
+            // 
+            this.labelProcessed.AutoSize = true;
+            this.labelProcessed.Location = new System.Drawing.Point(72, 102);
+            this.labelProcessed.Name = "labelProcessed";
+            this.labelProcessed.Size = new System.Drawing.Size(13, 13);
+            this.labelProcessed.TabIndex = 10;
+            this.labelProcessed.Text = "0";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(72, 122);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(30, 13);
+            this.labelTime.TabIndex = 11;
+            this.labelTime.Text = "Time";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Count";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Search for";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(75, 143);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(194, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxSearchReturn
+            // 
+            this.textBoxSearchReturn.Location = new System.Drawing.Point(75, 170);
+            this.textBoxSearchReturn.Name = "textBoxSearchReturn";
+            this.textBoxSearchReturn.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearchReturn.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 247);
+            this.Controls.Add(this.textBoxSearchReturn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.labelProcessed);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelFiles);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEnd);
+            this.Controls.Add(this.textBoxStart);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.TextBox textBoxStart;
+        private System.Windows.Forms.TextBox textBoxEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelFiles;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelProcessed;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxSearchReturn;
+    }
+}
+
---
+++ Implementation/VisualStudioStuff/CleanWikiParallel/CleanWikiParallel/CleanWikiParallel/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CleanWikiParallel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxPath.Text = @"D:\SoftwareProjects\SemanticRelations\Korpora\Wikipedia\pages\";
+            textBoxStart.Text = "0";
+            textBoxEnd.Text = "1";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private static List<string> GetFiles(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return Directory.GetFiles(path).ToList();
+            }
+            else
+            {
+                return new List<string>(); 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime time = DateTime.Now;
+            labelTime.Text = (DateTime.Now - time).ToString();
+            List<string> myEnumerable = new List<string>();
+            int start = 0;
+            Int32.TryParse(textBoxStart.Text, out start);
+            int end = 0;
+            Int32.TryParse(textBoxEnd.Text, out end);
+
+            for (int i = start; i < end; ++i)
+            {
+                myEnumerable.AddRange(GetFiles(textBoxPath.Text + i ));
+            }
+            labelFiles.Text = myEnumerable.Count + "";
+            /*
+            List<string> list = new List<string>();
+            for (int i = 0; i < 200; i++)
+            {
+                list.Add(myEnumerable.ElementAt(i));
+            }
+            */
+            Parallel.ForEach(myEnumerable, obj =>
+            {
+
+                //ProcessStartInfo perlStartInfo = new ProcessStartInfo(@"C:\Perl64\bin\perl.exe");
+                ProcessStartInfo perlStartInfo = new ProcessStartInfo(@"C:\Windows\System32\cmd.exe");
+                string output = " > "+Path.GetDirectoryName(obj)+@"\texts\text" + Path.GetFileName(obj) + ".txt";
+                perlStartInfo.Arguments = @"/c perl D:\SoftwareProjects\SemanticRelations\Einarbeitung\Word2Vec\Test1\wikicleaner.pl " + obj + output;
+                perlStartInfo.UseShellExecute = false;
+                perlStartInfo.RedirectStandardOutput = true;
+                perlStartInfo.RedirectStandardError = true;
+                perlStartInfo.CreateNoWindow = true;
+
+                Process perl = new Process();
+                perl.StartInfo = perlStartInfo;
+                perl.Start();
+                perl.WaitForExit();
+                output = perl.StandardOutput.ReadToEnd();
+                
+                int i;
+                Int32.TryParse(textBoxStart.Text, out i);
+
+            });
+            labelTime.Text = (DateTime.Now - time).ToString();
+            Count();
+            System.Console.WriteLine("Done.");
+        }
+
+        private void Count()
+        {
+            List<string> myEnumerable = new List<string>();
+            int start = 0;
+            Int32.TryParse(textBoxStart.Text, out start);
+            int end = 0;
+            Int32.TryParse(textBoxEnd.Text, out end);
+
+            for (int i = start; i < end; ++i)
+            {
+                myEnumerable.AddRange(GetFiles(textBoxPath.Text + i + @"\texts\"));
+            }
+            labelProcessed.Text = myEnumerable.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Count();
+        }
+        
+        static int _value;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<string> myEnumerable = new List<string>();
+            int start = 0;
+            Int32.TryParse(textBoxStart.Text, out start);
+            int end = 0;
+            Int32.TryParse(textBoxEnd.Text, out end);
+            string word = textBoxSearch.Text;
+
+            Parallel.For(start, end, num =>
+                {
+                    string fullpath = textBoxPath.Text + num + @"\texts\";
+                    //Thread newThread = new Thread(new ParameterizedThreadStart(Work.A));
+                    //newThread.Start(new Wrapper(fullpath, word));
+                    //newThread.Join();
+                    var ret = GetFiles(fullpath);
+                    int count = ret.Where(x => Path.GetFileName(x).StartsWith(word)).Count();
+
+                    Interlocked.Add(ref Form1._value, count);
+
+                    Console.WriteLine(Form1._value);
+                    
+                }
+                
+                );
+
+            //Thread thread1 = new Thread(Form1.A);
+            //Thread thread2 = new Thread(A);
+            //thread1.Start(1);
+            //thread2.Start(2);
+            //thread1.Join();
+            //thread2.Join();
+	        Console.WriteLine(Form1._value);
+            textBoxSearchReturn.Text = Form1._value.ToString();
+
+        }
+        public class Wrapper
+        {
+            public string mPath;
+            public string mWord;
+            public Wrapper(string path, string word)
+            {
+                mPath = path;
+                mWord = word;
+            }
+
+        }
+        public class Work
+        {
+            public static void A(object input)
+            {
+                var value= (Wrapper)input;
+
+                var ret = GetFiles(value.mPath);
+                int count = ret.Where(x=>x.StartsWith(value.mWord)).Count();
+
+                Interlocked.Add(ref Form1._value, count);
+            }
+
+        }
+        
+        
+    }
+}
